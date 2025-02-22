d50c79b4f5a8cb7d3cc635f4626ccbfa4002bce5
'Admin' <'jeyaraajaabijith@gmail.com'>
2025-02-22 15:39:57 +0530

    New file is added

---
+++ src/com/demo/MethodDemo.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+public class MethodDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		MethodDemo md = new MethodDemo();
+		md.printNumber();
+
+	}
+	
+	void printNumber() {
+		System.out.println("Number printed.....");
+	}
+
+}
